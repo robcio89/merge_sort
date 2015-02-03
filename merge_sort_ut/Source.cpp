38aef2b3e41c5ff1d76ef0ed12f70ed0435b8d34
robcio89 <robcio89@gmail.com>
2015-02-03 22:47:53 +0100

    remove comments

--- merge_sort_ut/Source.cpp
+++ merge_sort_ut/Source.cpp
@@ -1,19 +1,6 @@
-// This header file defines the public API for Google Test.
-//It should be included by any test program that uses Google Test.
 #include "gtest/gtest.h"
-
 #include "..\merge_sort_code\FileReader.h"
 
-//#include "..\Calc\Calc.h"
-
-/* TEST(test_case_name, test_name) is a predefined macro. 
-   These are ordinary C++ functions that don't return a value. 
-   In this function, along with any valid C++ statements you want to include, 
-   use the various Google Test assertions to check values. 
-   The test's result is determined by the assertions; 
-   if any assertion in the test fails (either fatally or non-fatally), 
-   or if the test crashes, the entire test fails. Otherwise, it succeeds.
-*/
 TEST(aaa, readFile)
 {
 	FileReader f;
